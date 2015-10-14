be52859c394536859ce2076de0197864fb020a8b
Luciekimotho <luciekimotho@gmail.com>
2015-10-14 09:06:35 +0300

    Added the algorithm for the roman conversion

--- roman/views.py
+++ roman/views.py
@@ -10,4 +10,30 @@ def romancode(request):
 	context = {
        "form": form
 	}
+
+	if form.is_valid():
+		roman_numeral = form.cleaned_data.get('roman_numeral')
+		translated = translate(roman_numeral)
+
 	return render(request, 'romancode.html', context)
+
+
+
+def translate(myroman):
+    myDict = {
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100 }
+
+    myroman = myroman.upper()
+    number = 0
+    while myroman:
+        if len(myroman) == 1 or myDict[myroman[0]] >= myDict[myroman[1]]:
+            number += myDict[myroman[0]]
+            myroman = roman[1:]
+        else:
+            number += myDict[myroman[1]] - myDict[myroman[0]]
+            myroman = myroman[2:]
+    print (number)
